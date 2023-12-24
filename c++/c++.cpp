2072c8797719bee6f9bf2d7ff5b859fafd1d56d4
Caner <Caner@DESKTOP-V48FJUH>
2023-12-24 18:31:55 +0300

    Proje dosyası ekle.

---
+++ c++/c++.cpp
@@ -0,0 +1,397 @@
+﻿
+
+//Caner Erdoğdu
+
+
+//#include <iostream>
+//#include <string>
+
+//using namespace std;
+
+//int main()
+//{
+
+	//string isim = "caner";		//isim ve soyismi yazdırır
+	//string soyisim = "erdogdu";
+
+	//cout << "isim: " << isim << soyisim<<endl;
+	
+
+
+
+	//string isim;		kullanıcıdan veri alır
+	//string soyisim;
+
+	//cout << "isim giriniz: "<<endl;
+	//cin >> isim;
+	//cout << "soyisim giriniz: "<<endl;
+	//cin >> soyisim;
+
+	//cout << "hosgeldiniz: " << isim + soyisim;
+
+
+
+
+
+
+	//string isim = "caner";	ekrana yazıyı yazdırır
+	//cout << isim;
+
+
+
+
+	//int s1 = 5; ekrana sayıyı yazdırır
+	//cout << s1;
+
+
+
+
+
+	//int a = 6;
+	//int b = 7;		toplama islemi yapar
+	//cout << a + b<<endl;
+
+
+
+
+	//int a;
+	//cout << "sayi giriniz ";		kullanıcıdan sayı alır
+	//cin >> a;
+
+
+
+
+	//string isim = "caner";
+	//string soyisim = "erdogdu";		birlestirip yazar
+	//cout << isim + soyisim;
+
+
+	//cout << "caner " << "erdogdu ";	2. yöntem birlestirmede 	
+
+
+
+	//string isim;
+	//cout << "isim giriniz: ";		kullanıcıdan isim alır
+	//cin >> isim;
+
+
+
+
+
+	//char harf = 'A';			char degiskeni tekli harflerde yazılır
+	//cout << harf;
+
+
+
+
+
+	//bool a = 5;		bool true yada false döndürür
+	//cout << a;
+
+
+
+
+	//double s1 = 1.7066;		double yüksek sayıları yazar flout da aynısıdır
+	//cout << s1;
+
+
+
+
+
+
+
+	//int s1, s2;
+	//cout << "sayi giriniz: ";			
+	//cin >> s1;	
+	//cout << "sayi giriniz: ";
+	//cin >> s2; 
+	//cout << "toplama islemi: " << s1 + s2 << endl;		4 islemi birlikte verir
+	//cout << "cikarma islemi: " << s1 - s2 << endl;
+	//cout << "carpma islemi: " << s1 * s2 << endl;
+	//cout << "bolme islemi: " << s1 / s2 << endl;
+
+
+
+	//int s1, s2, sonuc;
+	//char secim;
+	//cout << "secim yapiniz: +,-,*,/ ";	4 islem yapar secim ile
+	//cin >> secim;
+	//cout << "sayi giriniz: ";
+	//cin >> s1;
+	//cout << "sayi giriniz: ";
+	//cin >> s2;
+	//if (secim == '+') {
+		//sonuc = s1 + s2;
+	//}
+	//if (secim == '-') {
+	//	sonuc = s1 - s2;
+	//}
+	//if (secim == '*'){
+		//sonuc = s1*s2;
+	//}
+	//if (secim == '/') {
+		//sonuc = s1 / s2;
+	//}
+	//cout <<"sonuc: " << sonuc;
+
+
+
+	
+
+	//int s1, s2,s3;
+	//cout << "1. sayiyi gir: ";		hangi sayi buyuk onu bulur
+	//cin >> s1;
+	//cout << "2. sayiyi gir: ";
+	//cin >> s2;
+	//cout << "3. sayiyi gir: ";
+	//cin >> s3;
+
+	//if (s1 > s2 && s1 > s3) {
+	//	cout << "1. sayi buyuk ";
+	//}
+	//if (s2 > s1 && s2 > s3) {
+		//cout << "2. sayi buyuk ";
+	//}
+	//if (s3 > s1 && s3 > s2) {
+		//cout << "3. sayi buyuk ";
+	//}
+	//if (s1 == s2 && s1 == s3) {
+		//cout << "sayilar esit ";
+	//}
+
+
+
+	//char harf1,harf2;
+	//cout << "1. harfi giriniz: ";
+	//cin >> harf1;
+	//cout << "2. harfi giriniz: ";		hangi harf buyuk onu bulur
+	//cin >> harf2;
+
+	//if (harf1 < harf2) {
+		//cout << "1. harf buyuk"<<endl;
+	//}
+	//if (harf1 > harf2) {
+		//cout << "1. harf  kucuk"<<endl;
+	//}
+	//if (harf2 < harf1) {
+		//cout << "2. harf buyuk"<<endl;
+	//}
+	//if (harf2 > harf1) {
+		//cout << "2. harf kucuk"<<endl;
+	//}
+
+
+
+	//double karekokBul,s1;
+	//cout << "sayi giriniz: ";		sayinin karekokunu bulur
+	//cin >> s1;
+	//karekokBul = sqrt(s1);
+	//cout << "karekoku: " << karekokBul << endl;
+
+
+	//______________________________//
+
+
+	//string isim = "caner";
+	//string soyisim = "erdogdu";
+
+	//if (isim == "caner" && soyisim == "erdogdu")	eğer üstekki isim ve soyisim doğruysa yazdırır
+	//{
+		//cout << "basarili ";
+	//}
+	//else
+		//cout << "basarisiz";
+
+
+
+
+
+	//string isim;
+	//string soyisim;
+
+	//cout << "isim giriniz: ";				kullanıcıdan giris istiyoruz 
+	//cin >> isim;
+	//cout << "soyisim giriniz: ";
+	//cin >> soyisim;
+
+	//if (isim == "caner" && soyisim == "erdogdu") {
+		//cout << "giris basarili ";
+	//}
+	//else
+		//cout << "yanlis islem !!!";
+
+
+
+
+	
+
+	//string isim,soyisim;
+	//int parola;
+	//cout << "isim giriniz: ";
+	//cin >> isim;
+	//cout << "soyisim giriniz: ";		kullanıcıdan veri alma parola ile
+	//cin >> soyisim;
+	//cout << "parola giriniz: ";
+	//cin >> parola;
+	//if (isim == "caner" && soyisim == "soyisim") {
+		//cout << "hosgeldiniz: " << isim + soyisim << endl;
+	//}
+	//else if (parola == 1903) {
+	//	cout << "hosgeldiniz: "<<isim+soyisim<<endl;
+	//}
+	//else
+		//cout << "hatali !!!";
+
+
+
+
+
+
+	//int s1;
+	//cout << "sayi tahmin et ";	baska bir ifade
+	//cin >> s1;
+
+	//if (s1 == 5) {
+		//cout << "bildin ";
+	//}
+	//else
+		//cout << "bilemedin ";
+
+
+
+//__________________________________//
+
+
+	//not: for da int icine yazılır
+	
+	//for (int i = 0; i < 10; ++i) {		1 den 9' a kadar yazar;
+	//cout << i<<endl;
+	//}
+
+
+
+
+	//for (int i = 0; i <= 10; ++i) {		10 tane caner yazar
+	//cout << "caner"<<endl;
+	//}
+
+
+
+
+	//for (int i = 0; i <= 3; ++i) {		hafta ve gun yazar
+		//cout << "hafta: "<<endl;
+
+	//	for (int j = 0; j <= 7; ++j) {
+			//cout << "gun: "<<endl;
+
+		//}
+	//}
+
+
+
+
+
+//int i = 0;			do while döngüsüyle 4 sefer yazı yazdırma
+//
+//do {
+//	cout << "caner" << endl;
+//
+//	
+//	
+//	i++;
+//} while (i < 5);
+
+
+
+
+
+
+//int i = 0;		do while döngüsü
+//do {
+//	cout << i << "\n";
+//	i++;
+//} while (i < 5);
+
+
+
+
+
+	//_________________________//
+
+
+	//not: while da int dışarıda yazılır
+
+
+	//int i = 0;
+	//while (i <= 10) {		10 a kadar yazar
+	//cout << i << endl;
+	//++i;
+	//}
+
+
+
+	//int i = 0;
+	//while (i <= 10) {		10 defa caner yazdırır
+	//cout << "caner"<<endl;
+	//++i;
+	//}
+
+
+
+	//_________________________//
+
+
+
+//int secim;
+//cout << "secim yapiniz: ";
+//cin >> secim;
+//switch (secim) {
+//case 1:
+	//cout << "pazartesi ";		1 e basarsan pazartesi 2 ye basarsan sali tercih yaptırma
+	//break;
+//case 2:
+	//cout << "sali ";
+	//break;
+	//}
+
+
+
+
+//int tercih;
+//cout << "1'e basarsan yuruyerek gidersin: "<<endl<<"2'e basarsan arabayla gidersin: "<<endl;
+//cin >> tercih;
+
+//switch (tercih) {
+//case 1:
+	//cout << "yuruyerek ";
+	//break;
+//case 2:
+	//cout << "arabayla ";
+	//break;
+
+//}
+
+
+
+//_____________________________________//
+
+
+//string araclar[3] = { "volvo","ford","mercedes" };	indeks numarasını aldı
+//cout<< araclar[1];
+
+
+
+
+//string cars[5] = { "Volvo", "BMW", "Ford", "Mazda", "Tesla" };	0 dan 4 e kadar sıralar
+//for (int i = 0; i < 5; i++) {
+//	cout << i << " = " << cars[i] << "\n";
+//}
+
+
+
+
+
+//}
+
+
+
